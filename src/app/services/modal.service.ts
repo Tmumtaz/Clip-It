b520b1d4e98d9a7d19288782e85860141d7d2300
Taimur Mumtaz <taimur.mumtaz0@gmail.com>
2023-05-12 15:36:09 -0400

    modal converted into re-useable modal

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
-  private visible = false;
+  private modals: IModal[] = []
 
   constructor() {}
+
+  register(id: string){
+    this.modals.push({
+      id,
+      visible: false
+    }) 
+  }
+
   // method to return the visible value
-  isModalOpen() {
-    return this.visible;
+  isModalOpen(id: string, ) : boolean {
+    return !!this.modals.find(element => element.id === id)?.visible
   }
 
   //toggle visible property
-  toggleModal() {
-    this.visible = !this.visible;
+  toggleModal(id : string) {
+    const modal = this.modals.find(element => element.id === id)
+
+    if(modal){
+      modal.visible = !modal.visible
+    }
   }
 }
