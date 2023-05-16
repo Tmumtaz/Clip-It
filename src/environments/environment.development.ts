07d1c8c36c0b685d406dca7b03885969ed0c8d6e
Taimur Mumtaz <taimur.mumtaz0@gmail.com>
2023-05-16 14:30:40 -0400

    angular fire added

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase : {
+        apiKey: "AIzaSyDT4mWYFe91J2jfHD6RhqJBH5Qyr9OimOo",
+        authDomain: "clip-it-51bab.firebaseapp.com",
+        projectId: "clip-it-51bab",
+        storageBucket: "clip-it-51bab.appspot.com",
+        messagingSenderId: "937484271015",
+        appId: "1:937484271015:web:6c11acbeb302ab91a14a82"
+      }
+};
+
