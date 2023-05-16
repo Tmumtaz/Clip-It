009d3bce79b6b0293dddcce921693d944e32b352
Taimur Mumtaz <taimur.mumtaz0@gmail.com>
2023-05-16 11:11:38 -0400

    alert component for register login form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -32,6 +32,9 @@ export class RegisterComponent {
     Validators.minLength(12),
     Validators.maxLength(12)
   ])
+  showAlert = false;
+  alertMsg = 'Please wait, your account is being created.'
+  alertColor = 'blue'
 
   registerForm = new FormGroup({
    name: this.name,
@@ -43,6 +46,8 @@ export class RegisterComponent {
   })
 
   register(){
-    console.log('register called')
+    this.showAlert = true
+    this.alertMsg = 'Please wait, your account is being created.'
+    this.alertColor = 'blue'
   }
 }
