c317b4756335b90b2927eb4530505aadb0b346ef
Taimur Mumtaz <taimur.mumtaz0@gmail.com>
2023-05-17 14:32:47 -0400

    modal delay + logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { Observable,  } from 'rxjs';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import IUser from '../models/user.model';
-import { map } from 'rxjs/operators';
+import { map, delay } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private usersCollection: AngularFirestoreCollection<IUser>
-  public isAuthenticated$ : Observable<boolean>
+  private usersCollection: AngularFirestoreCollection<IUser>;
+  public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
-    this.usersCollection = db.collection('users')
-    this.isAuthenticated$= auth.user.pipe(
-      map(user => !!user)
-    )
+    this.usersCollection = db.collection('users');
+    this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
   }
 
   public async createUser(userData: IUser) {
-    if(!userData.password) {
-      throw new Error("Password not provided")
+    if (!userData.password) {
+      throw new Error('Password not provided');
     }
 
     //Authenticate user
@@ -31,8 +34,8 @@ export class AuthService {
     );
 
     //conditional statement to ensure user is not null
-    if(!userCred.user){
-      throw new Error('User cant be found')
+    if (!userCred.user) {
+      throw new Error('User cant be found');
     }
     //add document to collection
     await this.usersCollection.doc(userCred.user?.uid).set({
@@ -43,7 +46,7 @@ export class AuthService {
     });
 
     await userCred.user.updateProfile({
-      displayName: userData.name
-    })
+      displayName: userData.name,
+    });
   }
 }
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,18 +1,44 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   credentials = {
     email: '',
     password: '',
-  }
+  };
 
-  login(){
-    console.log(this.credentials)
+  showAlert = false;
+  alertMsg = 'Please Wait, we are logging you in';
+  alertColor = 'blue';
+  inSubmission = false;
+
+  constructor(private auth: AngularFireAuth) {}
+
+  async login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please Wait, we are logging you in';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (error) {
+      this.inSubmission = false;
+      this.alertMsg = 'An Error Occured, please try again';
+      this.alertColor = 'red';
+      return
+    }
+
+    this.alertMsg = 'Login Successful!';
+    this.alertColor = 'green';
+
   }
-  
 }
