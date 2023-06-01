1a79ea50c78b5975d9e2c3d9f842779455484b14
Taimur Mumtaz <taimur.mumtaz0@gmail.com>
2023-06-01 11:12:25 -0400

    clips list linked on homepage

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ClipService } from '../services/clip.service';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
-  styleUrls: ['./clips-list.component.css']
+  styleUrls: ['./clips-list.component.css'],
 })
-export class ClipsListComponent {
+export class ClipsListComponent implements OnInit, OnDestroy {
+  constructor(public clipService: ClipService) {
+    this.clipService.getClips()
+  }
 
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const {scrollTop, offsetHeight} = document.documentElement
+    const { innerHeight } = window
+
+    const bottomofWindow = Math.round(scrollTop) + innerHeight === offsetHeight
+
+    if(bottomofWindow){
+      this.clipService.getClips()
+    }
+  };
 }
