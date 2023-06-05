36006db56fee29313e54039740c0729341556ed8
Taimur Mumtaz <taimur.mumtaz0@gmail.com>
2023-06-05 13:06:22 -0400

    images rendering ( need to adjust size), timestamp format altered with new pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,8 @@ import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { ClipsListComponent } from './clips-list/clips-list.component'
+import { ClipsListComponent } from './clips-list/clips-list.component';
+import { FbTimestampPipe } from './pipes/fb-timestamp.pipe'
 
 ModalComponent
 @NgModule({
@@ -27,6 +28,7 @@ ModalComponent
     ClipComponent,
     NotFoundComponent,
     ClipsListComponent,
+    FbTimestampPipe,
   ],
   imports: [
     BrowserModule,
